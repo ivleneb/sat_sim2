f922c55da612f34f16d0341cd64b1d9ad8559f10
ivleneb <beenelvi.godoy@gmail.com>
2022-05-25 04:10:58 +0000

    macros in server logs

--- src/sateliteemulator/sateliteemulator.c
+++ src/sateliteemulator/sateliteemulator.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/socket.h> // 
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <signal.h> // SIGINT
 #include <string.h> // bzero
 #include <time.h>
+
+char log_buffer[256]={0};
+char name_buffer[100]={0};
+#define LOG(str) strncpy(log_buffer, str, sizeof(log_buffer)), log_buffer[255]='\0', printf("[%s]: %s.\n", name_buffer, log_buffer)
+#define LOG2(str, n) sprintf(log_buffer, str, n), printf("[%s]: %s.\n", name_buffer, log_buffer)
+#define LOG3(str, n, m) sprintf(log_buffer, str, n, m), printf("[%s]: %s.\n", name_buffer, log_buffer)
+#define LOG4(str, n, m, o) sprintf(log_buffer, str, n, m, o), printf("[%s]: %s.\n", name_buffer, log_buffer)
 
 struct tsk{
 	char name[50];
@@ -23,48 +30,51 @@ void intHandler(int dummy){
 int main(int argc, char **argv){
 	srand(time(NULL));
 	if (argc!=3){
-		printf("SAT: BAD call. Must be '%s name port'.\n", argv[0]);
+		printf("[SAT]: BAD call. Must be '%s name port'.\n", argv[0]);
 		return -1;
 	}
-	printf("SAT %s: Iniciando en puerto %s\n", argv[1], argv[2]);
+	strncpy(name_buffer, argv[1], sizeof(name_buffer));
+	name_buffer[99]='\0';
+
+	LOG2("Iniciando en puerto %s", argv[2]);
 	int portno = 0;
 	if((portno=atoi(argv[2]))==0){
-		printf("SAT %s: BAD value for port.\n", argv[1]);
+		LOG("BAD value for port");
 		return -2;
 	}
 	//printf("Puerto: %d\n", portno);
 	int server_fd;
-	
+
 	if((server_fd=socket(AF_INET, SOCK_STREAM, 0))==0){
-		printf("SAT %s: Socket FAIL.\n", argv[1]);
+		LOG("Socket FAIL");
 		return -3;
 	} //else printf("SAT %s: Socket inicializado.\n", argv[1]);
-	
+
 	struct sockaddr_in address;
 	bzero(&address, sizeof(address));
 	address.sin_family= AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY; // localhost // htonl(INADDR_ANY)
 	address.sin_port = htons(portno);
-	
+
 	// socket to address
 	if(bind(server_fd, (struct sockaddr *) &address, sizeof(address))<0){
-		printf("SAT %s: Bind FAIL.\n", argv[1]);
+		LOG("Bind FAIL");
 		return -4;
 	} //else printf("SAT %s: Bind ok.\n", argv[1]);
-	
+
 	// socket listening
 	if(listen(server_fd, 3)<0){
-		printf("SAT %s: Listen FAIL.\n", argv[1]);
+		LOG("Listen FAIL");
 		return -5;
-	} else printf("SAT %s: Listening ...\n", argv[1]);
-	
+	} else LOG("Listening ...");
+
 	int new_socket;
 	int addrlen = sizeof(address);
 	if((new_socket=accept(server_fd, (struct sockaddr *) &address, (socklen_t*)  &addrlen))<0){
-		printf("SAT %s: Accept FAIL.\n", argv[1]);
+		LOG("Accept FAIL");
 		return -6;
-	} else printf("SAT %s: Socket paired.\n", argv[1]);
-	
+	} else LOG("Socket paired");
+
 	char buffer[1024]={0};
 	int valread;
 	signal(SIGINT, intHandler);
@@ -72,27 +82,29 @@ int main(int argc, char **argv){
 	while(keepRunning){
 		struct tsk tarea;
 		if((valread=read(new_socket, &tarea, sizeof(tarea)))>0){
-			printf("SAT %s: Len: %d.\n", argv[1], valread);
-			printf("SAT %s: %s %.1f [", argv[1], tarea.name, tarea.pyf);
+			LOG2("Len: %d", valread);
 			int j;
-			for(j=0; j<tarea.num_res; j++) printf("%d,", tarea.resources[j]);
-			printf("]\n");
-			printf("SAT %s: procesando ... \n", argv[1]);
+			char res_buf[100]={0};
+			int n=0;
+			for(j=0; j<tarea.num_res; j++) n=sprintf(res_buf+n, "%d,", tarea.resources[j]);
+			LOG4("%s %.1f [%s]", tarea.name, tarea.pyf, res_buf);
+			LOG("Procesando ..");
 			bzero(buffer, 1024);
 			int rand_res=rand()%10;
 			if(rand_res==5) sprintf(buffer, "%s", "FAIL");
 			else sprintf(buffer, "%s", "SUCCESS");
+			LOG("Enviando respuesta ..");
 			if(write(new_socket, buffer, 200)!=200){
-				printf("SAT %s: Fallo envio.\n", argv[1]);
+				LOG("Fallo envio");
 			}
 		} else {
-			printf("SAT %s: Error on read.\n", argv[1]);
+			LOG("Error on read");
 			break;
 		}
 	}
-	printf("SAT %s: Cerrando conexión...\n", argv[1]);
-	if(close(new_socket)!=0) printf("SAT %s: Socket pair to cliente fail to close.\n", argv[1]);
-	if(close(server_fd)!=0) printf("SAT %s: Socket server  fail to close.\n", argv[1]);
-	printf("SAT %s: END.\n", argv[1]);
+	LOG("Cerrando conexión..");
+	if(close(new_socket)!=0) LOG("Socket pair to client fail to close");
+	if(close(server_fd)!=0) LOG("Socket server  fail to close");
+	LOG("END");
 	return 0;
 }
